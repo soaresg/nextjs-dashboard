d0c6bc424073b289e07b71fa1e877968badb8be2
Gabriel Soares <desenvolvedorgabrielsoares@gmail.com>
2025-05-09 15:41:57 -0300

    Change in this commit:     .:: fonts.ts created and imported Inter and Lusitana Google Fonts     .:: Alter in the layout app the font     .:: Insert images in the app page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import '@/app/ui/global.css';
 import React from "react";
+import {inter} from "@/app/ui/fonts";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+export default function RootLayout({children}: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
